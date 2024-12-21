f579d7a51579b14061fb686b8959148014bd9c54
Ganesh Gowda M <ganeshgowdam6@gmail.com>
2024-12-21 10:24:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+pickle_in = open("./classifier.pkl", "rb")
