983511b71155bfc83abfe5536026e7d16cc43800
Ganesh Gowda M <ganeshgowdam6@gmail.com>
2024-12-22 18:23:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
